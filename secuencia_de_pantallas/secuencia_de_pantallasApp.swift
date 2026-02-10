5e8ddb080ebcd98a2e40e47d0d5d61fbe0e190ca
D310820 <tecnocel30210@gmail.com>
2026-02-10 11:29:09 -0600

    Initial Commit

---
+++ secuencia_de_pantallas/secuencia_de_pantallasApp.swift
@@ -0,0 +1,17 @@
+//
+//  secuencia_de_pantallasApp.swift
+//  secuencia_de_pantallas
+//
+//  Created by Jose Daniel Espinoza Gomez on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct secuencia_de_pantallasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
